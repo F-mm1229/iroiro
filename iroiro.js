9d61efe52111b7d6ce1aa63b3026651610fdb630
F-mm1229 <meru112mofmof129@gmail.com>
2022-04-14 23:48:10 +0900

    The color can be changed random with the button.

--- iroiro.js
+++ iroiro.js
@@ -1,8 +1,13 @@
 const button = document.querySelector('.button');
 
-document.getElementById('target').addEventListener('click',function(){
-    document.getElementById('color-1').style.background = '#0b6e4f';
-    document.getElementById('color-2').style.background = '#2b4162';
-    document.getElementById('color-3').style.background = '#fa9f42';
-    document.getElementById('color-4').style.background = '#721817';
+document.getElementById('target').addEventListener('click', function () {
+    const randomColor1 = '#' + Math.random().toString(16).slice(-6);
+    const randomColor2 = '#' + Math.random().toString(16).slice(-6);
+    const randomColor3 = '#' + Math.random().toString(16).slice(-6);
+    const randomColor4 = '#' + Math.random().toString(16).slice(-6);
+    
+    document.getElementById('color-1').style.background = randomColor1;
+    document.getElementById('color-2').style.background = randomColor2;
+    document.getElementById('color-3').style.background = randomColor3;
+    document.getElementById('color-4').style.background = randomColor4;
 })
