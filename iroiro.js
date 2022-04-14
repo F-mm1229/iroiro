4da1ee46bc7ab9865a4cf943efa469f47644af27
F-mm1229 <meru112mofmof129@gmail.com>
2022-04-14 23:25:14 +0900

    The color can be changed with the button.

---
+++ iroiro.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.button');
+
+document.getElementById('target').addEventListener('click',function(){
+    document.getElementById('color-1').style.background = '#0b6e4f';
+    document.getElementById('color-2').style.background = '#2b4162';
+    document.getElementById('color-3').style.background = '#fa9f42';
+    document.getElementById('color-4').style.background = '#721817';
+})
