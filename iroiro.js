f7ad4da9810773e41b199491e708f1cd0e3e9d32
F-mm1229 <meru112mofmof129@gmail.com>
2022-04-17 02:14:20 +0900

    Can return to previous color.

--- iroiro.js
+++ iroiro.js
@@ -1,12 +1,6 @@
-const button = document.querySelector('.button');
-
-// ボタンを押すことで色をランダムに変更する
-document.getElementById('generate').addEventListener('click', function () {
-    for (let i = 1; i <= 4; i++) {
-        const randomColor = Math.random().toString(16).slice(-6);
-        document.getElementById('color-' + i).style.background = '#' + randomColor;
-    }
-});
+var color = [];         // カラーコードを格納
+let count = 0;          //generateした回数
+let backcount = 0;      //reverseした回数
 
 // グリッド1
 document.getElementById('grid1').addEventListener('click', function () {
@@ -23,3 +17,29 @@ document.getElementById('grid2').addEventListener('click', function () {
         document.getElementById('color-' + i).style.height = '100%';
     }
 });
+
+// 変更ボタンで色をランダムに変更する
+document.getElementById('generate').addEventListener('click', function () {
+    count += 1;
+    backcount = 0;
+    for (let i = 1; i <= 4; i++) {
+        const randomColor = Math.random().toString(16).slice(-6);
+        document.getElementById('color-' + i).style.background = '#' + randomColor;
+        
+        color.push(randomColor);
+    }
+    console.log(color);
+    console.log(count);
+});
+
+// 戻るボタンで前に出た色に戻る
+document.getElementById('reverse').addEventListener('click', function () {
+    backcount += 1;
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById('color-' + i).style.background = '#' + color[color.length - ((4 + 1 - i) + 4 * backcount)];
+    }
+    if (backcount >= count - 1) return;
+    console.log(backcount);
+})
+
+
